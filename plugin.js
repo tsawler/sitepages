eadd5b32a56054e333ba07a4a088c8e1d0481e7f
Trevor Sawler <trevor.sawler@gmail.com>
2015-12-18 09:51:44 -0400

    Formatting

--- plugin.js
+++ plugin.js
@@ -1,13 +1,15 @@
-CKEDITOR.plugins.add( 'sitepages', {
+CKEDITOR.plugins.add('sitepages', {
     icons: 'sitepages',
-    init: function( editor ) {
-        editor.addCommand( 'sitepages', new CKEDITOR.dialogCommand( 'sitepagesDialog' ) );
-        editor.ui.addButton( 'Sitepages', {
+    init: function(editor) {
+        editor.addCommand('sitepages', new CKEDITOR.dialogCommand(
+            'sitepagesDialog'));
+        editor.ui.addButton('Sitepages', {
             label: 'Insert link to site page',
             command: 'sitepages',
             toolbar: 'insert'
         });
 
-        CKEDITOR.dialog.add( 'sitepagesDialog', this.path + 'dialogs/sitepages.js' );
+        CKEDITOR.dialog.add('sitepagesDialog', this.path +
+            'dialogs/sitepages.js');
     }
 });
